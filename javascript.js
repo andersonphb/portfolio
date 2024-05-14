11e0a077fa57f48a10be76b15e0e65bedae0d3a3
Anderson <164803886+andersonphb@users.noreply.github.com>
2024-05-14 20:38:59 -0300

    Delete javascript.js

--- javascript.js
+++
@@ -1,7 +0,0 @@
-let trilho = document.getElementById('trilho')
-let body = document.querySelector('body')
-
-trilho.addEventListener('click', ()=>{
-    trilho.classList.toggle('dark')
-    body.classList.toggle('dark')
-})
